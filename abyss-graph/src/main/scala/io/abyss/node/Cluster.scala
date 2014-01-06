6a8ec828ea11c7b7a421fda099956306031e0b4d
cane <cane@iqtech.pl>
2014-01-06 12:34:49 +0100

    Refactoring and cleaning

--- abyss-graph/src/main/scala/io/abyss/node/Cluster.scala
+++ abyss-graph/src/main/scala/io/abyss/node/Cluster.scala
@@ -18,14 +18,102 @@
 
 package io.abyss.node
 
+// Created by cane, 1/6/14 12:25 PM
+
 import akka.actor.Address
-import akka.cluster.{MemberStatus, Member}
+import akka.cluster.{MemberStatus, Member, Cluster}
+import akka.cluster.ClusterEvent._
+import io.abyss.{Abyss, AbyssActor}
+
+class ClusterManager extends AbyssActor with ClusterMembersCollector {
 
 
-/**
- * Created by cane, 28.06.13 21:58
- * $Id: ClusterMembersCollector.scala,v 1.2 2013-12-31 21:09:28 cane Exp $
- */
+
+	/**
+	 * Set to true when OnMemberUp happened, otherwise is false. If node was previously Up then send current
+	 * cluster state to NodeManager each time member is moved to Up or Removed state.
+	 */
+	var memberWasUp = false
+
+
+	// -------------------------------------------------------------------------------------------------------
+
+
+	Cluster (context.system) registerOnMemberUp {
+		// when node comes up it sends current cluster state to node manager, which takes actions
+		log.info ("I am up, starting roles: {}", nodeRoles.mkString(","))
+		cluster sendCurrentClusterState Abyss.nodeManager
+		memberWasUp = true
+	}
+
+
+	// -------------------------------------------------------------------------------------------------------
+
+
+	/**
+	 * Starting procedure:
+	 * - subscribe to ClusterDomainEvent messages
+	 */
+	override def preStart (): Unit = {
+		Cluster (context.system).subscribe (self, classOf[ ClusterDomainEvent ])
+	}
+
+
+	/**
+	 * Stopping procedure
+	 * - unsubscribe from events
+	 */
+	override def postStop (): Unit = {
+		Cluster (context.system).unsubscribe (self)
+	}
+
+
+	def receive = {
+
+		case MemberUp (member) =>
+			log.debug ("Member is Up: {}", member.address)
+			memberUp (member)
+			if(memberWasUp) cluster sendCurrentClusterState Abyss.nodeManager
+
+		case MemberRemoved (member, previousStatus) =>
+			log.debug ("Member is Removed: {} after {}", member.address, previousStatus)
+			memberRemoved (member, previousStatus)
+			if(memberWasUp) cluster sendCurrentClusterState Abyss.nodeManager
+
+
+		// TODO below processed events has no Abyss meaning now
+
+		case UnreachableMember (member) =>
+			// prepare for changes in cluster (for example degrade shards to write-through cache)
+			log.debug ("Member detected as unreachable: {}", member)
+
+
+		case LeaderChanged (newLeader) =>
+			log.debug ("Leader changed: {}", newLeader.toString)
+			leaderChanged (newLeader)
+
+
+		case RoleLeaderChanged (role, leader) =>
+			log.debug ("Role {} leader changed to {}", role, leader.toString)
+			roleLeaderChanged(role, leader)
+
+
+		case msg: CurrentClusterState =>
+			log.info("Cluster state: {}", msg.toString)
+
+		case msg: ClusterMetricsChanged =>
+
+		// print out rest of ClusterDomainEvent messages
+		case msg: ClusterDomainEvent =>
+			log.debug ("Domain event not processed: %s" format msg.toString)
+	}
+
+}
+
+
+
+
+
 trait ClusterMembersCollector  {
 
 
--- abyss-graph/src/main/scala/io/abyss/node/Node.scala
+++ abyss-graph/src/main/scala/io/abyss/node/Node.scala
@@ -20,6 +20,79 @@ package io.abyss.node
 
 import java.util.concurrent.ConcurrentHashMap
 import io.abyss.graph.model.GraphElement
+import akka.actor.{FSM, Props}
+import akka.cluster.ClusterEvent.CurrentClusterState
+import io.abyss.node.front.FrontendManager
+import io.abyss.node.data.DataManager
+import io.abyss._
+import io.abyss.client._
+
+
+trait NodeState
+
+case object Initializing extends NodeState
+
+case object Working extends NodeState
+
+
+trait NodeData
+
+case object NoData extends NodeData
+
+case class WorkingData(lastClusterState: CurrentClusterState,
+					   lastAbyssClusterState: AbyssClusterState) extends NodeData
+
+/**
+ * Starts node roles actors and forwards some messages to them.
+ *
+ * Created by cane, 22.06.13 17:50
+ */
+class NodeManager
+	extends AbyssActor
+	with FSM[NodeState, NodeData] {
+
+	// TODO these actors should be replaced with some kind of proxy when not in role
+
+	/**
+	 * Frontend manager
+	 */
+	val frontRole = context.actorOf (Props[FrontendManager], FrontRoleName)
+
+	/**
+	 * Backend data manager
+	 */
+	val dataRole = context.actorOf (Props[DataManager], DataRoleName)
+
+
+	startWith(Initializing, NoData)
+
+	when(Initializing) {
+		case Event(currentClusterState: CurrentClusterState, NoData) =>
+			val acs = AbyssClusterState (currentClusterState, None)
+
+			// propagate Abyss Cluster State to role-bound actors
+			if (nodeRoles.contains (FrontRoleName)) frontRole forward acs
+			if (nodeRoles.contains (DataRoleName)) dataRole forward acs
+
+			goto(Working) using WorkingData(currentClusterState, acs)
+	}
+
+	when(Working) {
+		case Event(msg: ClientSpawned, sd: WorkingData) =>
+			sender ! AbyssFrontMembers(sd.lastAbyssClusterState.frontNodes)
+			stay()
+
+		case Event(currentClusterState: CurrentClusterState, sd: WorkingData) =>
+			val acs = AbyssClusterState (currentClusterState, Some(sd.lastClusterState))
+
+			// TODO reconfigure node due to cluster membership changes (may possibly result in some kind of maintenance state)
+			stay() using WorkingData(currentClusterState, acs)
+	}
+
+
+
+}
+
 
 
 /**
